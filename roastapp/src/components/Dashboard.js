781bcfeb2ff598cca2eedcc1b6e62e43e0def003
Joelheile <61017303+Joelheile@users.noreply.github.com>
2023-02-09 19:05:00 +0100

    fetch projects works

--- roastapp/src/components/Dashboard.js
+++ roastapp/src/components/Dashboard.js
@@ -1,9 +1,30 @@
-import React from "react";
+import { collection, getDocs } from "firebase/firestore";
+import React, { useState, useEffect } from "react";
+import { db } from "../firebase";
 
-export default function NewProject() {
+export default function Dashboard() {
+	const [projects, setProjects] = useState([]);
+	const projectsCollectionRef = collection(db, "projects");
+	useEffect(() => {
+		// called when page renders
+
+		const getProjects = async () => {
+			const data = await getDocs(projectsCollectionRef);
+			setProjects(data.docs.map((doc) => ({ ...doc.data(), id: doc.id }))); // we only want name and id
+		};
+		getProjects();
+	}, []);
+
 	return (
 		<div>
 			<h1>Dashbaord</h1>
+			{projects.map((projects) => {
+				return (
+					<div>
+						<h1>{projects.title}</h1>
+					</div>
+				);
+			})}
 		</div>
 	);
 }
--- roastapp/src/components/NewProject.js
+++ roastapp/src/components/NewProject.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles.css";
+//import { db } from "../firebase";
 
 export default function NewProject() {
 	return (
@@ -7,8 +8,10 @@ export default function NewProject() {
 			<body>
 				<div class="row">
 					<div class="column">
-						<h1>New Roast</h1>
-						<button class="button">Upload Files</button>
+						<div>
+							<h1>New Roast</h1>
+							<button class="button">Upload Files</button>
+						</div>
 					</div>
 					<div class="column">
 						<h1>col2</h1>
