1996c747140d8940d3665e6442833e95efbf23f6
Joelheile <61017303+Joelheile@users.noreply.github.com>
2023-02-12 15:34:08 +0100

    SignIn & Navbar designed

--- roastapp/src/components/auth/SignIn.jsx
+++ roastapp/src/components/auth/SignIn.jsx
@@ -29,7 +29,44 @@ const SignIn = () => {
 } 
 
   return (
-    <div className="">
+    <div >
+<div class="w-screen flex  justify-center items-center ">
+  <form class="bg-white  rounded px-8 pt-6 pb-8 mb-4">
+    <div class="mb-4 ">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+        Username
+      </label>
+      <input class="shadow appearance-none border rounded  py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Enter your email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}/>
+    </div>
+    <div class="mb-6">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+        Password
+      </label>
+      <input class="shadow appearance-none border  rounded py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Enter your password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)} />
+      
+    </div>
+    <div class="flex items-center justify-between">
+      <button class="bg-primary hover:bg-primaryLight text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" onClick={signIn}>
+        Sign In
+      </button>
+      <a class="inline-block align-baseline font-bold text-sm text-primary hover:text-primaryLight"  onClick={signUpNav}>
+       No account?
+      </a>
+    </div>
+  </form>
+  
+</div>
+    </div>
+  );
+};
+export default SignIn;
+
+/*
+<div className="">
       <form onSubmit={signIn}>
       <h1 className="text-primary">
       Hello world!
@@ -52,6 +89,5 @@ const SignIn = () => {
       <button onClick={signUpNav}>Not signed in yet?</button>
 
     </div>
-  );
-};
-export default SignIn;
+
+    */
--- roastapp/src/components/auth/SignUp.jsx
+++ roastapp/src/components/auth/SignUp.jsx
@@ -26,12 +26,49 @@ const SignUp = () => {
       })
       .catch((error) => {
         console.log(error);
-        alert("Sorry there is already an account with this mail")
+        alert("There is already an account with this mail")
+        navigate("/")
       });
   };
 
   return (
-    <div className="sign-in-container">
+    <div >
+<div class="w-screen flex  justify-center items-center ">
+  <form class="bg-white  rounded px-8 pt-6 pb-8 mb-4">
+    <div class="mb-4 ">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+        Username
+      </label>
+      <input class="shadow appearance-none border rounded  py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Enter your email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}/>
+    </div>
+    <div class="mb-6">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+        Password
+      </label>
+      <input class="shadow appearance-none border  rounded py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Enter your password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)} />
+      
+    </div>
+    <div class="flex items-center justify-between">
+      <button class="bg-primary hover:bg-primaryLight text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" onClick={signUp}>
+        Create Account
+      </button>
+      
+    </div>
+  </form>
+  
+</div>
+    </div>
+    
+  );
+};
+export default SignUp;
+
+/*
+<div className="sign-in-container">
       <form onSubmit={signUp}>
         <h1>Create Account</h1>
         <input
@@ -49,6 +86,4 @@ const SignUp = () => {
         <button type="submit">Sign Up</button>
       </form>
     </div>
-  );
-};
-export default SignUp;
+*/
